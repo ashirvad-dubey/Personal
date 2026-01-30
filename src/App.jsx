1e66fb012e7e6ba698ea5644b0c0048f9e7a9b88
Ashirvad Kumar Dubey <ashirvaddubey8@gmail.com>
2026-01-30 11:55:21 +0530

    Testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import Home from "./Pages/Home";
+import Home from "./pages/Home";
 import Personal from "./pages/Personal";
 import Photo from "./pages/Photo";
 import Admin from "./pages/Admin";
